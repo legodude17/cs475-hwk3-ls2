fb342fa9685b7c9b3bba55a081981084c46dac77
Joshua <legodude17@users.noreply.github.com>
2024-02-08 09:40:05 -0800

    Initial commit

---
+++ ls2.c
@@ -0,0 +1,18 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "ls2.h"
+
+
+// TODO: function definitions here for ls2
+
+/**
+ * This is just an example. Delete this before 
+ * submission.
+ */
+void example(int* x) {
+    *x = thisIsGlobal;
+} 
---
+++ ls2.h
@@ -0,0 +1,17 @@
+#ifndef LS2_H_INCLUDED
+#define LS2_H_INCLUDED
+
+#include "stack.h"
+
+#define TRUE 1
+#define FALSE 0
+#define INDENT "    "
+
+// TODO: Any global variables go below
+int thisIsGlobal = 10;  // delete before submission
+
+// TODO: Function declarations go below
+void example(int*); // delete before submission
+
+
+#endif
